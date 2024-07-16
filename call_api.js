0324664cfe0201c890a6623d505707d86a9f71e6
Adrien VERGNAUD <a.vergnaud@cat-amania.com>
2024-07-16 15:58:50 +0200

    evol conversation

--- call_api.js
+++ call_api.js
@@ -15,9 +15,37 @@ const perplexity = createOpenAI({
     baseURL: BASE_URL,
 });
 
-const { text } = await generateText({
-    model: perplexity(MODEL),
-    prompt: 'Write a vegetarian lasagna recipe for 4 people.',
+async function call_api(input) {
+    const { text } = await generateText({
+        model: perplexity(MODEL),
+        messages: input,
+    });
+    return text;
+}
+
+const conversation = [
+    { 
+        'role': 'system', 
+        'content': 'Soit précis et concis.' 
+    },
+    { 
+        'role': 'user', 
+        'content': 'Le terrain de mon voisin n\'est pas entretenu. Quelles démarches je peux entreprendre ?' 
+    },
+  ];
+
+const reponse1 = await call_api(conversation);
+console.log(reponse1);
+
+conversation.push({
+    'role': 'assistant',
+    'content': reponse1
 });
 
-console.log(text);
+conversation.push({
+    'role': 'user',
+    'content': 'Quels articles du code civil peuvent être pertinents dans ce cas'
+});
+
+const reponse2 = await call_api(conversation);
+console.log(reponse2);
